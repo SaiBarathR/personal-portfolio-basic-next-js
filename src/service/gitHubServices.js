d39eadd032d36588083246b90a3db9a09ec397b9
Sai Barath <saibarathdeep@outlook.com>
2023-07-06 17:15:35 +0530

    added github api to get user,repo details

---
+++ src/service/gitHubServices.js
@@ -0,0 +1,18 @@
+export async function getGithubUserDetails(githubUser) {
+    return fetch(`https://api.github.com/users/${githubUser}`, {
+        username: githubUser,
+        headers: {
+            'X-GitHub-Api-Version': '2022-11-28'
+        }
+    }).then((response) => response.json()).then((data) => data);
+}
+
+export async function getRepos(githubUser) {
+    return fetch(`https://api.github.com/users/${githubUser}/repos`, {
+        owner: 'OWNER',
+        repo: 'REPO',
+        headers: {
+            'X-GitHub-Api-Version': '2022-11-28'
+        }
+    }).then((response) => response.json()).then((data) => data);
+}
