aeeec4df2580c9e7b0236ff67a4b1f42a8ac8a1e
Sai Barath <saibarathdeep@outlook.com>
2023-07-08 02:18:55 +0530

    added filler contents for github and css sites

--- src/app/home/PortfolioHome.jsx
+++ src/app/home/PortfolioHome.jsx
@@ -2,6 +2,7 @@ import Intro from "./Intro";
 import Image from "next/image";
 import LinkedInShowcase from "./LinkedInShowCase";
 import Description from "./Description";
+import FillerContent from "./FillerContent";
 
 export default function PortfolioHome() {
     return (
@@ -9,6 +10,7 @@ export default function PortfolioHome() {
             <Intro />
             <LinkedInShowcase />
             <Description />
+            <FillerContent/>
         </>
     )
 }
