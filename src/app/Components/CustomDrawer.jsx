cf046233bc8c58a4d22e1ca9dcd593aab0623f0f
Sai Barath <saibarathdeep@outlook.com>
2023-07-09 03:38:34 +0530

    added custom drawer

---
+++ src/app/Components/CustomDrawer.jsx
@@ -0,0 +1,33 @@
+'use client'
+import styled from "@emotion/styled";
+import { Box, SwipeableDrawer, useMediaQuery } from "@mui/material";
+import { grey } from '@mui/material/colors';
+
+function SwipeableEdgeDrawer({ open, toggleDrawer, position, ...props }) {
+    const iOS = typeof navigator !== 'undefined' && /iPad|iPhone|iPod/.test(navigator.userAgent);
+    const isMobile = useMediaQuery('(max-width:767px)')
+
+    return (
+        <SwipeableDrawer
+            anchor={position}
+            open={open}
+            onClose={toggleDrawer(false)}
+            onOpen={toggleDrawer(true)}
+            disableSwipeToOpen={false}
+            disableBackdropTransition={!iOS}
+            disableDiscovery={iOS}
+            transitionDuration={{
+                appear: 500,
+                enter: 500,
+                exit: 500,
+            }}
+            PaperProps={{ sx: { width: isMobile ? "100%" : "auto" } }}
+        >
+            {props.children}
+        </SwipeableDrawer>
+
+    );
+}
+
+
+export default SwipeableEdgeDrawer;
