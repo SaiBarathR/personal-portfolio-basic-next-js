3121a7c0c320ea02542b8f7b43ae65df491f48ea
Sai Barath <saibarathdeep@outlook.com>
2023-07-10 18:58:17 +0530

    added new colors to getintouch

--- src/app/home/GetInTouch.jsx
+++ src/app/home/GetInTouch.jsx
@@ -11,7 +11,7 @@ import Github from '../../icons/github.svg'
 
 export default function GetInTouchDrawer({ toggleDrawer, open }) {
 
-    return <div className={" w-full md:w-auto bg-white  min-h-[200px] flex flex-row  h-full " + space_grotesk.className}>
+    return <div className={" w-full bg-teal-50 md:w-auto min-h-[200px] flex flex-row  h-full " + space_grotesk.className}>
         <div className="w-[6px] h-[30px] bg-slate-400 rounded-lg flex self-center items-center m-2 cursor-pointer " onMouseDown={toggleDrawer(false)}></div>
         <div className="w-full flex flex-col m-2 mr-4 md:m-10 md:mr-12 gap-9 items-center ">
             <h1 className="font-semibold text-3xl ">Contact - Info</h1>
@@ -38,7 +38,7 @@ function GeitInTouchContent({ open }) {
     }
 
     function ImageRenderer({ src, link, name, className }) {
-        return <div className={"flex hover:bg-slate-200  md:m-0 m-2 flex-col md:flex-row gap-5  items-center cursor-pointer hover:scale-105 transition ease-in-out delay-75 hover:-translate-y-1 shadow-md p-2 rounded-lg " + className}
+        return <div className={"flex hover:bg-cyan-200  md:m-0 m-2 flex-col md:flex-row gap-5  items-center cursor-pointer hover:scale-105 transition ease-in-out delay-75 hover:-translate-y-1 shadow-md p-2 rounded-lg " + className}
             onClick={() => onClickContact(link, name)}
         >
             <Image src={src} alt={name} />
@@ -46,9 +46,9 @@ function GeitInTouchContent({ open }) {
         </div >
     }
 
-    return <div className={`w-full flex flex-col shadow-2xl p-4 md:p-6  gap-5 items-start rounded-2xl mb-5 overflow-y-auto ${open ? "animate-appear" : "animate-disappear"}`}>
+    return <div className={`w-full flex flex-col bg-cyan-100 shadow-2xl p-4 md:p-6  gap-5 items-start rounded-2xl mb-5 overflow-y-auto ${open ? "animate-appear" : "animate-disappear"}`}>
         <div className="w-full flex h-min flex-col md:flex-row  items-center gap-5 ">
-            <Image src={ProfileEmoji} alt={"profil"} className=" bg-slate-100 h-[80px] w-[80px] rounded-full flex items-center justify-center pl-2 pt-1" />
+            <Image src={ProfileEmoji} alt={"profil"} className=" bg-teal-200 h-[80px] w-[80px] rounded-full flex items-center justify-center pl-2 pt-1" />
             <div>
                 <h1 className="font-semibold text-2xl">Sai Barath R</h1>
                 <p className="font-light text-xs">Software Engineer - RND</p>
