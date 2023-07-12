5d35d9e8deb2ef056b6f9713a2da41c4b99823fc
Sai Barath <saibarathdeep@outlook.com>
2023-07-12 18:35:15 +0530

    added github api

--- src/service/gitHubServices.js
+++ src/service/gitHubServices.js
@@ -5,8 +5,25 @@ export async function getGithubUserDetails(githubUser) {
     }).then((response) => response.json()).then((data) => data);
 }
 
+export async function getGithubUserSocialAcoounts(githubUser) {
+    return fetch(`https://api.github.com/users/${githubUser}/social_accounts`, {
+        username: githubUser,
+        headers: { 'X-GitHub-Api-Version': '2022-11-28' }
+    }).then((response) => response.json()).then((data) => data);
+}
+
 export async function getRepos(githubUser) {
     return fetch(`https://api.github.com/users/${githubUser}/repos`, {
+        owner: 'OWNER',
+        repo: 'REPO',
+        headers: {
+            'X-GitHub-Api-Version': '2022-11-28'
+        }
+    }).then((response) => response.json()).then((data) => data);
+}
+
+export async function getweeklyCommitCount(githubUser) {
+    return fetch(`https://api.github.com/repos/${githubUser.owner}/${githubUser.repo}/stats/participation`, {
         owner: 'OWNER',
         repo: 'REPO',
         headers: {
