4cd0146787b8a4a75ca7c0dc0561f5c81e0d78a3
Sai Barath <saibarathdeep@outlook.com>
2023-07-12 07:38:18 +0530

    added file loader config

--- next.config.js
+++ next.config.js
@@ -1,4 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {}
 
-module.exports = nextConfig
+module.exports = {
+    webpack: (config, options) => {
+        config.module.rules.push({
+            test: /\.(pdf)$/,
+            type: 'asset',
+            generator: {
+                filename: 'static/chunks/[path][name].[hash][ext]'
+            },
+        });
+
+        return config;
+    }
+};
