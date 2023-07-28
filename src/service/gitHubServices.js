24a9c397b34dcedb02ed28f353600458c1c0b655
Sai Barath <saibarathdeep@outlook.com>
2023-07-28 16:33:34 +0530

    added new api to get specific repo

--- src/service/gitHubServices.js
+++ src/service/gitHubServices.js
@@ -22,6 +22,16 @@ export async function getRepos(githubUser) {
     }).then((response) => response.json()).then((data) => data);
 }
 
+export async function getRepoDetails(repoInfo) {
+    return fetch(`https://api.github.com/repos/${repoInfo.owner}/${repoInfo.name}`, {
+        owner: 'OWNER',
+        repo: 'REPO',
+        headers: {
+            'X-GitHub-Api-Version': '2022-11-28'
+        }
+    }).then((response) => response.json()).then((data) => data);
+}
+
 export async function getweeklyCommitCount(githubUser) {
     return fetch(`https://api.github.com/repos/${githubUser.owner}/${githubUser.repo}/stats/participation`, {
         owner: 'OWNER',
